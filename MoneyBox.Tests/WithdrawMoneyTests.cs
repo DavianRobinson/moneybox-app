5d2bb0cfb3ce1ccc1edad53cd6a8f45cd87d333c
DavianRobinson <davianr@gmail.com>
2021-03-17 14:20:27 +0000

    added withdrawa money tests and method boday

---
+++ MoneyBox.Tests/WithdrawMoneyTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Xunit;
+using Moneybox.App.Domain;
+using Moneybox.App.Features;
+using Moneybox.App.DataAccess;
+using Moq;
+using Moneybox.App.Domain.Services;
+
+namespace Moneybox.Tests
+{
+    public class WithdrawMoneyTests
+    {
+        private readonly User user = new User(Guid.NewGuid(), "testing@email.com", "Test");
+
+        public WithdrawMoneyTests()
+        {
+
+        }
+        [Fact]
+        public void WithdrawMoney()
+        {
+
+            //arrange
+
+            var TestAccount = new Account(user);
+            TestAccount.Credit(1000m);
+
+            var accountRepo = new Mock<IAccountRepository>();
+            accountRepo.Setup(x => x.GetAccountById(TestAccount.Id)).Returns(TestAccount);
+
+            var notificationService = new Mock<INotificationService>();
+
+            var sut = new WithdrawMoney(accountRepo.Object, notificationService.Object);
+
+
+            //act
+            sut.Execute(TestAccount.Id, 10m);
+
+            //assert
+            Assert.Equal(990m, TestAccount.Balance);
+        }
+        [Fact]
+        public void WithdrawMoneyZeroBalance()
+        {
+            //arrange
+
+            var TestAccount = new Account(user);
+            TestAccount.Credit(0m);
+
+            var accountRepo = new Mock<IAccountRepository>();
+            accountRepo.Setup(x => x.GetAccountById(TestAccount.Id)).Returns(TestAccount);
+            var notificationService = new Mock<INotificationService>();
+
+            var sut = new WithdrawMoney(accountRepo.Object, notificationService.Object);
+            //act
+            //assert
+            Assert.Throws<InvalidOperationException>(() => sut.Execute(TestAccount.Id, 10m));
+
+        }
+        [Fact]
+        public void WithdrawMoneyInsufficientFunds()
+        {
+            //arrange
+
+            var TestAccount = new Account(user);
+            TestAccount.Credit(5m);
+
+            var accountRepo = new Mock<IAccountRepository>();
+            accountRepo.Setup(x => x.GetAccountById(TestAccount.Id)).Returns(TestAccount);
+            var notificationService = new Mock<INotificationService>();
+
+            var sut = new WithdrawMoney(accountRepo.Object, notificationService.Object);
+
+            //act
+            //assert
+            Assert.Throws<InvalidOperationException>(() => sut.Execute(TestAccount.Id, 10m));
+        }
+
+        [Fact]
+        public void WithdrawMoneyFundsLowLimit()
+        {
+            //arrange
+
+            var TestAccount = new Account(user);
+            TestAccount.Credit(500m);
+
+            var accountRepo = new Mock<IAccountRepository>();
+            accountRepo.Setup(x => x.GetAccountById(TestAccount.Id)).Returns(TestAccount);
+            var notificationService = new Mock<INotificationService>();
+
+            var sut = new WithdrawMoney(accountRepo.Object, notificationService.Object);
+
+            //act
+            sut.Execute(TestAccount.Id, 10m);
+
+            //assert
+            notificationService.Verify(x => x.NotifyFundsLow(user.Email));
+
+        }
+    }
+}
